51ce2367d1091da89076ff19a0e392c32dcd67e4
malena <89429614+malenarivera@users.noreply.github.com>
2023-07-05 17:31:57 -0300

    Add files via upload

--- estructuras/grafo/Grafo.java
+++ estructuras/grafo/Grafo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package universidad.estructuras.conjuntistas;
+package universidad.estructuras.grafo;
 import universidad.estructuras.dinamica.Lista;
 
 /**
